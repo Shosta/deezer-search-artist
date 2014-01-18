81c34a12db01b5b92079358ad5f3795570adb0cb
Shosta <remilavedrine-git@yahoo.fr>
2014-01-18 14:46:37 +0100

    Initial Commit

---
+++ Deezer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Deezer
+//
+//  Created by Rems on 18/01/2014.
+//  Copyright (c) 2014 RemiLavedrine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeezerTests/DeezerTests.m
@@ -0,0 +1,34 @@
+//
+//  DeezerTests.m
+//  DeezerTests
+//
+//  Created by Rems on 18/01/2014.
+//  Copyright (c) 2014 RemiLavedrine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DeezerTests : XCTestCase
+
+@end
+
+@implementation DeezerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
